bf4d275b82d58c21957e64a5990d30de892498ba
suwapotta <nguyenduchthientan09@gmail.com>
2026-01-29 05:36:36 +0700

    [niri, nvim] Add keybind to hide top bar, make completion menu transparent

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -7,5 +7,13 @@ return {
       transparent = true,
       solid = true,
     },
+
+    -- Force completion menu background to be transparent
+    custom_highlights = function(colors)
+      return {
+        Pmenu = { bg = "none" },
+        PmenuSel = { bg = colors.surface0, fg = "none" },
+      }
+    end,
   },
 }
