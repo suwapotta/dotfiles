28cf13ff4c042395271c8cf06b44081207702980
suwapotta <nguyenduchthientan09@gmail.com>
2026-02-07 21:33:27 +0700

    [fish] Add paru search function

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -56,6 +56,18 @@ function pacmanQ
     pacman -Qq | fzf --multi --query "$argv" --preview 'pacman -Qi {1}'
 end
 
+function paruS
+    set keyword $argv
+
+    if test -z "$keyword"
+        read -P (set_color -o blue)"::"(set_color -o normal)" Search AUR: " keyword
+    end
+
+    if test -n "$keyword"
+        paru -Ssq "$keyword" | fzf --multi --preview 'paru -Si {1}' | xargs -ro paru -S
+    end
+end
+
 if status is-interactive # Commands to run in interactive sessions can go here
 
     # No greeting
