2a05f188e251bcd92c0c3fcc142577e32846a95f
suwapotta <nguyenduchthientan09@gmail.com>
2026-03-01 08:25:04 +0700

    [fish+starship] Add transient prompt

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -13,6 +13,7 @@ if status is-interactive # Commands to run in interactive sessions can go here
 
     # Use starship
     starship init fish | source
+    enable_transience
     if not set -q TMUX
         if test -f ~/.local/state/quickshell/user/generated/terminal/sequences.txt
             cat ~/.local/state/quickshell/user/generated/terminal/sequences.txt
